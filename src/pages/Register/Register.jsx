e3254f79dc8d68e97c8f143b8d82e5f6c0a8ab11
impressivecoder01 <rohanrahman251@gmail.com>
2024-12-19 01:52:10 +0600

    lottie added successfully

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,19 +1,17 @@
+import Lottie from "lottie-react";
 import React from "react";
-
+import registerLottieData from '../../assets/Lottiefiles/Animation - 1734550880947.json'
 const Register = () => {
   return (
-    <div>
+    <div className="max-w-6xl mx-auto">
       <div className="hero bg-base-200 min-h-screen">
         <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">Login now!</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
+          <div className="text-center lg:text-left w-96">
+           
+            <Lottie animationData={registerLottieData}></Lottie>
           </div>
           <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <h1 className="text-5xl font-bold text-center py-4">Register Now!</h1>
             <form className="card-body">
               <div className="form-control">
                 <label className="label">
@@ -43,7 +41,7 @@ const Register = () => {
                 </label>
               </div>
               <div className="form-control mt-6">
-                <button className="btn btn-primary">Login</button>
+                <button className="btn btn-primary">Register</button>
               </div>
             </form>
           </div>
