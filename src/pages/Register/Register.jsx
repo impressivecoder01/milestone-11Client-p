43dd539555cd5e65ad789f7b388515a3733f7664
impressivecoder01 <rohanrahman251@gmail.com>
2024-12-19 02:11:27 +0600

    collect form data and varified password

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,18 +1,30 @@
 import Lottie from "lottie-react";
 import React from "react";
-import registerLottieData from '../../assets/Lottiefiles/Animation - 1734550880947.json'
+import registerLottieData from "../../assets/Lottiefiles/Animation - 1734550880947.json";
 const Register = () => {
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
+    const passwordRegex = /^(?=.*[A-Z])(?=.*\d).{6,}$/;
+    if(!passwordRegex.test(password)){
+        alert('password should be at least 6 character, and one upper case ')
+    }
+  };
   return (
     <div className="max-w-6xl mx-auto">
       <div className="hero bg-base-200 min-h-screen">
         <div className="hero-content flex-col lg:flex-row-reverse">
           <div className="text-center lg:text-left w-96">
-           
             <Lottie animationData={registerLottieData}></Lottie>
           </div>
           <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-          <h1 className="text-5xl font-bold text-center py-4">Register Now!</h1>
-            <form className="card-body">
+            <h1 className="text-5xl font-bold text-center py-4">
+              Register Now!
+            </h1>
+            <form onSubmit={handleRegister} className="card-body">
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
@@ -20,6 +32,7 @@ const Register = () => {
                 <input
                   type="email"
                   placeholder="email"
+                  name="email"
                   className="input input-bordered"
                   required
                 />
@@ -31,6 +44,7 @@ const Register = () => {
                 <input
                   type="password"
                   placeholder="password"
+                  name="password"
                   className="input input-bordered"
                   required
                 />
