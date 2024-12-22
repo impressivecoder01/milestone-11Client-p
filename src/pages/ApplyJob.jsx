8d11f1eb660b87dc5af4b93f051df2c296bd26c8
impressivecoder01 <rohanrahman251@gmail.com>
2024-12-23 01:18:39 +0600

    post job applicant data successfully

--- src/pages/ApplyJob.jsx
+++ src/pages/ApplyJob.jsx
@@ -1,76 +1,83 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useParams } from "react-router-dom";
+import { AuthContext } from "../Provider/AuthProvider";
 
 const ApplyJob = () => {
-    const {id} = useParams()
-    console.log(id)
-    const submitApplication = e => {
-        e.preventDefault()
-        const form = e.target
-        const linkedIn = form.name.value;
-        const github = form.name.value;
-        const resume = form.resume.value;
-        console.log(linkedIn, github, resume)
-    }
+  const { id } = useParams();
+  const { user } = useContext(AuthContext);
+  //   console.log(id,user);
+  const submitApplication = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const linkedIn = form.name.value;
+    const github = form.name.value;
+    const resume = form.resume.value;
+    // console.log(linkedIn, github, resume);
+    const jobApplication = {
+      job_id: id,
+      applicant_email: user.email,
+      linkedIn,
+      github,
+      resume,
+    };
+    fetch('http://localhost:5000/jobs_application',{
+        method: 'POST',
+        headers: {
+            'content-type' : 'application/json'
+        },
+        body: JSON.stringify(jobApplication)
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+    })
+  };
   return (
     <div>
-      <div className="hero bg-base-200 min-h-screen">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">Login now!</h1>
-            <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
+      <div className="card bg-base-100 max-w-6xl mx-auto  shadow-2xl">
+        <h1 className="text-5xl font-bold text-center">Submit now!</h1>
+        <form onSubmit={submitApplication} className="card-body">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">LinkedIn</span>
+            </label>
+            <input
+              type="url"
+              placeholder="LinkedIn ID"
+              name="linkedIn"
+              className="input input-bordered"
+              required
+            />
           </div>
-          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-            <form onSubmit={submitApplication} className="card-body">
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">LinkedIn</span>
-                </label>
-                <input
-                  type="url"
-                  placeholder="LinkedIn ID"
-                  name="linkedIn"
-                  className="input input-bordered"
-                  required
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">GitHub</span>
-                </label>
-                <input
-                  type="url"
-                  placeholder="GitHub"
-                  name="github"
-                  className="input input-bordered"
-                  required
-                />
-                
-              </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">GitHub</span>
+            </label>
+            <input
+              type="url"
+              placeholder="GitHub"
+              name="github"
+              className="input input-bordered"
+              required
+            />
+          </div>
 
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Resume</span>
-                </label>
-                <input
-                  type="url"
-                  placeholder="Resume"
-                  name="resume"
-                  className="input input-bordered"
-                  required
-                />
-                
-              </div>
-              <div className="form-control mt-6">
-                <button className="btn btn-primary">Login</button>
-              </div>
-            </form>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Resume</span>
+            </label>
+            <input
+              type="url"
+              placeholder="Resume"
+              name="resume"
+              className="input input-bordered"
+              required
+            />
           </div>
-        </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-primary">Submit </button>
+          </div>
+        </form>
       </div>
     </div>
   );
--- src/pages/HotJobCards.jsx
+++ src/pages/HotJobCards.jsx
@@ -13,42 +13,47 @@ const HotJobCards = ({ job }) => {
     description,
     company,
     requirements,
-    company_logo, _id
+    company_logo,
+    _id,
   } = job;
   return (
     <div>
       <Link to={`jobs/${_id}`}>
-      <div className="card bg-base-100  shadow-xl">
-        <div className="flex items-center py-1 gap-2">
-        <figure>
-          <img
-          className="w-9 h-9"
-            src={company_logo}
-            alt="Shoes"
-          />
-        </figure>
-        <div className="flex items-center gap-2">
-            <h5 className="text-2xl font-bold">{company}</h5>
-            <p>{location}</p>
-        </div>
-        </div>
-        
-        <div className="card-body">
-          <h2 className="card-title">
-            {title}
-            <div className="badge badge-secondary">NEW</div>
-          </h2>
-          <p>{description}</p>
-          <div className="card-actions justify-end">
-            <div className="flex gap-2 flex-wrap ">
-                {
-                    requirements.map((requirement , index) => <p key={index} className="border rounded-md hover:bg-slate-500 hover:text-white hover:cursor-pointer text-center">{requirement}</p>)
-                }
+        <div className="card bg-base-100  shadow-xl">
+          <div className="flex items-center py-1 gap-2">
+            <figure>
+              <img className="w-9 h-9" src={company_logo} alt="Shoes" />
+            </figure>
+            <div className="flex items-center gap-2">
+              <h5 className="text-2xl font-bold">{company}</h5>
+              <p>{location}</p>
             </div>
-            <p >Salary: {salaryRange.min} - {salaryRange.max} {salaryRange.currency}</p>
+          </div>
+
+          <div className="card-body">
+            <h2 className="card-title">
+              {title}
+              <div className="badge badge-secondary">NEW</div>
+            </h2>
+            <p>{description}</p>
+            <div className="card-actions justify-end">
+              <div className="flex gap-2 flex-wrap ">
+                {requirements.map((requirement, index) => (
+                  <p
+                    key={index}
+                    className="border rounded-md hover:bg-slate-500 hover:text-white hover:cursor-pointer text-center"
+                  >
+                    {requirement}
+                  </p>
+                ))}
+              </div>
+              <p>
+                Salary: {salaryRange.min} - {salaryRange.max}{" "}
+                {salaryRange.currency}
+              </p>
+            </div>
           </div>
         </div>
-      </div>
       </Link>
       {/* <div className="card bg-base-100  shadow-xl">
         <div className="flex items-center py-1 gap-2">
